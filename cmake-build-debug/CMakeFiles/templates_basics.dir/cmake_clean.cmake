0841360580d9da243e8bc10dd2dc0c41300fec44
vikramchopde <chopde.vikram@gmail.com>
2017-07-09 10:06:35 +0800

    initial Commit

---
+++ cmake-build-debug/CMakeFiles/templates_basics.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/templates_basics.dir/main.cpp.o"
+  "templates_basics.pdb"
+  "templates_basics"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/templates_basics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
